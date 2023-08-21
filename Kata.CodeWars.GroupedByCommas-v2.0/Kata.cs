f8a3850e0d23888290761d234821d7c2bcf3de45
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-08-21 15:24:23 -0500

    Add project files.

---
+++ Kata.CodeWars.GroupedByCommas-v2.0/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.CodeWars.GroupedByCommas_v2._0
+{
+    public static class Kata
+    {
+        public static string GroupByCommas(int n)
+        {
+            return String.Format("{0:n0}", n);
+        }
+    }
+}
